ec01885085f730f0166893bd955fcd254bb8ee94
linuxuser1jeeplambo9 <jeeplambo9@gmail.com>
2024-11-26 16:02:31 -0500

    Create grabber.py

---
+++ grabber.py
@@ -0,0 +1,16 @@
+from flask import flask, request, redirect 
+from datetime import datetime
+import requests
+
+
+app = Flask (__name__)
+
+
+@app.route("/")
+def index():
+  return "Hello World!"
+
+
+
+if __name__== "__main__"
+app.run(host='0.0.0.0'
